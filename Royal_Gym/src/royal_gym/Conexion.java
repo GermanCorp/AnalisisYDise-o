d353e4a0af1f59c4e2a552e74aa92aacb9a5b73f
alxcr <alxcr@SONY-VAIO>
2018-02-21 19:25:42 -0600

    Metodo de insertar cliente

---
+++ Royal_Gym/src/royal_gym/Conexion.java
@@ -0,0 +1,57 @@
+package royal_gym;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexion {
+
+    private Connection conexion;
+    private Statement statement;
+
+    // método para conectarse a la base de datos
+    public void conectar() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conexion = DriverManager.getConnection("jdbc:sqlite:gimnasio.db");
+            statement = conexion.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public int guardar( String sql ) throws SQLException{
+        return statement.executeUpdate(sql);
+    }
+    
+    // método para cerrar la base de datos
+    public void cerrar(){
+        try {
+            conexion.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    // Método para insertar
+	public void insertarCliente(String nombres, String apellidos, String fechaNacimiento, String altura, String peso) {
+		try {
+			String sql = "insert into cliente (nombres, apellidos, fechaNacimiento, altura, peso) values (?,?,?,?,?)";
+			PreparedStatement consulta = conexion.prepareStatement(sql);
+			consulta.setString(1, nombres);
+			consulta.setString(2, apellidos);
+			consulta.setString(3, fechaNacimiento);
+			consulta.setString(4, altura);
+			consulta.setString(5, peso);
+			consulta.execute();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
