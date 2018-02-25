1c8ef7bc2e2f4c72de304a732542bf05666b96a2
JCPD95 <36673378+JCPD95@users.noreply.github.com>
2018-02-25 13:56:46 -0600

    Adicion de clase Validaciones

--- Royal_Gym/src/royal_gym/GUI_Principal.java
+++ Royal_Gym/src/royal_gym/GUI_Principal.java
@@ -26,6 +26,11 @@ public class GUI_Principal extends javax.swing.JFrame {
         });
 
         jButton3.setText("Inventario");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -59,6 +64,12 @@ public class GUI_Principal extends javax.swing.JFrame {
         ventanaRegistrarCliente.setVisible(true);
         ventanaRegistrarCliente.setLocationRelativeTo(this);
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        GUI_Inventario ventanaRegistrarInventario = new GUI_Inventario();
+        ventanaRegistrarInventario.setVisible(true);
+        ventanaRegistrarInventario.setLocationRelativeTo(this);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
